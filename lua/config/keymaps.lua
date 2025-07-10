0f4ed5c52415cf6ebeef81c200d9f90183d9b776
Naman Kashyap <nkashyap@g2.com>
2025-07-10 10:43:35 +0530

    Add ,, custom keymap Milestone: First cutom keymap!

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", ",,", "A,<ESC>")
+vim.keymap.set("i", ",,", "<ESC>A,")
+
+-- Great Resources:
+-- * https://www.reddit.com/r/neovim/comments/1k4efz8/share_your_proudest_config_oneliners/
