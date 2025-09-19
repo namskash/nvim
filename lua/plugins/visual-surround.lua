c1f7112d2dcc1871661c6374f93756e529879f93
Naman Kashyap <nkashyap@g2.com>
2025-09-19 12:18:25 +0530

    Visal surround yayyy
    * Tried mini.surround - didn't work (keymap conflict probably)
    * Tried nvim-surround - Worked if you pressed the keystrokes fast. But the keystrokes were too complicated...
    * Visual surround, just rightgit add lua/plugins/visual-surround.lua

---
+++ lua/plugins/visual-surround.lua
@@ -0,0 +1,16 @@
+return {
+  "NStefan002/visual-surround.nvim",
+  config = function()
+    require("visual-surround").setup({
+      -- if set to false, the user must manually add keymaps
+      use_default_keymaps = true,
+      -- will be ignored if use_default_keymaps is set to false
+      surround_chars = { "{", "}", "[", "]", "(", ")", "'", '"', "`", "<", ">" },
+      -- delete surroundings when the selection block starts and ends with surroundings
+      enable_wrapped_deletion = false,
+      -- whether to exit visual mode after adding surround
+      exit_visual_mode = true,
+    })
+    -- [optional] custom keymaps
+  end,
+}
