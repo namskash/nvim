7576c895ad35e7d579784a0e06025d1d4bd8f8d6
Naman Kashyap <nkashyap@g2.com>
2025-09-19 12:16:23 +0530

    Custom Keymmaps - small modification

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set("i", ",,", "<ESC>A,")
 
 vim.keymap.set("n", "ycc", "yygccp", { remap = true })
 
-vim.keymap.set("n", "c'\"", 'r"f\'r"', { remap = true }) -- Type c ' " to change from ' to "
-vim.keymap.set("n", "c\"'", "r'f\"r'", { remap = true }) -- Type c " ' to change from " to '
+vim.keymap.set("n", "c'\"", 'r"f\'r"<ESC>', { remap = true }) -- Type c ' " to change from ' to "
+vim.keymap.set("n", "c\"'", "r'f\"r'<ESC>", { remap = true }) -- Type c " ' to change from " to '
 
 -- Great Resources:
 -- * https://www.reddit.com/r/neovim/comments/1k4efz8/share_your_proudest_config_oneliners/
