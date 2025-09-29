81caa9330741be44d90e2528c1e5bc6c5fb16af9
Naman Kashyap <nkashyap@g2.com>
2025-09-29 10:54:44 +0530

    Re-enable & setup Copilot auto-suggestions/snippets in blink

--- lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
--- Disable plugins
-return {
-  { "zbirenbaum/copilot.lua", enabled = false },
-  { "zbirenbaum/copilot-cmp", enabled = false },
-  { "giuxtaposition/blink-cmp-copilot", enabled = false },
-}
