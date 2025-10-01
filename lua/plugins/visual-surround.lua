66b6124842f920eed0fe9d1a81183dfd458e0688
Naman Kashyap <nkashyap@g2.com>
2025-10-01 19:08:46 +0530

    Modify vim-surround keystrokes * This allows me to continue to use the }, ), ... ones for navigation

--- lua/plugins/visual-surround.lua
+++ lua/plugins/visual-surround.lua
@@ -5,7 +5,7 @@ return {
       -- if set to false, the user must manually add keymaps
       use_default_keymaps = true,
       -- will be ignored if use_default_keymaps is set to false
-      surround_chars = { "{", "}", "[", "]", "(", ")", "'", '"', "`", "<", ">" },
+      surround_chars = { "{", "[", "(", "'", '"', "`", "<", ">" },
       -- delete surroundings when the selection block starts and ends with surroundings
       enable_wrapped_deletion = false,
       -- whether to exit visual mode after adding surround
